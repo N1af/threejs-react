fa6ddc46997d130da9d6bbfb559d39d08321f627
N1af <naflanm060@gmail.com>
2025-04-16 22:04:31 +0530

    backend

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
-function App() {
+
+const App = () => {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    // Fetch data from the Node.js API
+    axios.get('http://localhost:5000/api/data')
+      .then(response => {
+        setData(response.data);
+      })
+      .catch(error => {
+        console.error('There was an error fetching data!', error)
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>
+    <h1>Data from mySql</h1>
+    <ul>
+      {data.map((item) => {
+        <li key={item.id}>{item.name} - {item.age}</li>
+      })}
+    </ul>
+   </div>
   );
 }
 
